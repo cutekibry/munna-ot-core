84b290e5b4433b156ad29ed15f6a5178c91a1436
cutekibry <cutekibry@gmail.com>
2024-07-23 17:04:16 +0800

    feat: export more types and classes
    Export more to make it easier to test c/s status
    and operation information.

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,8 @@
-export { Client } from "./client";
+export { Client, ClientStateSync, ClientStateAwait, ClientStateBuffered } from "./client";
+export type { ClientState } from "./client";
 
-export { Operation } from "./operation";
+export type { BasicOperation } from "./operation";
+export { Operation, isDelete, isInsert, isRetain, isEmptyBasicOp } from "./operation";
 
 import Server from "./server";
 export { Server };
