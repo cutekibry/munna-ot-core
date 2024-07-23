a61b7b104955e1b218339464236952123ad56d04
cutekibry <cutekibry@users.noreply.github.com>
2024-07-23 09:00:29 +0000

    deploy: 963b7e905faaa8f604814a05d62c5e2fd87f4941

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJyzslMzStR0lEqSCzJULJSSs5JLC5OLdaHiOtllOTmKOkoZWfmpShZGRpZ1OrAdfoXpBYllmTm52Fqhkvh0x+cWlSWWoSpGSKOqTMWAOv4qFS1AAAA"
